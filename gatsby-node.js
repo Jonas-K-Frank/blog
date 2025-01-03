d0931396a46a60636f8dbd4622aff66527c80336
Jonas Frank <jonas@jkfrank.dk>
2025-01-03 16:05:42 +0100

    billederne til forsiden driller

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,23 @@
 // const path = require('path')
 
+
+exports.createSchemaCustomization = ({ actions }) => {
+    const { createTypes } = actions;
+    createTypes(`
+      type Mdx implements Node {
+        frontmatter: Frontmatter
+      }
+      type Frontmatter {
+        title: String
+        slug: String
+        image: File @fileByRelativePath
+        date: Date @dateformat
+        author: String
+        category: String
+        readTime: Int
+      }
+    `);
+  };
 // // create pages dynamically
 // exports.createPages = async ({ graphql, actions }) => {
 //   const { createPage } = actions
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,41 @@ import Hero from "../components/Hero";
 import Layout from "../components/Layout";
 import Posts from "../components/Posts";
 import { graphql } from "gatsby";
-const IndexPage = () => {
+const IndexPage = ({data}) => {
+	const {allMdx:{nodes:posts}} = data;
 	return (
 		<Layout>
 			<Hero showPerson />
+			<Posts posts={posts} title="Nyeste indlæg" />
 		</Layout>
 	);
 };
+
+
+export const query = graphql`
+  {
+    allMdx(limit: 3, sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        id
+        frontmatter {
+          title
+          author
+          date(formatString: "DD MM YYYY")
+          category
+          slug
+          image {
+            childImageSharp {
+              gatsbyImageData(
+                width: 600
+                placeholder: BLURRED
+                formats: [AUTO, WEBP, AVIF]
+              )
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage;
